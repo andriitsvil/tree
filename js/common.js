875f129e6477a4110797541bccd8c256f51949b4
Andrii Tsvil <andriytsvil@gmail.com>
2018-04-29 00:43:12 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,12 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  
-//  document.getElementById("reset").addEventListener("click", function(){
-//    document.getElementById("stage-1").classList.remove("disable");
-//    document.getElementById("main").nextSibling.classList.add("disable");
-//  });
-  
- // console.log(document.getElementById("stage-1").nextElementSiblings);
-  
   document.getElementById("stage-1-unit-1").addEventListener("click", function(){
     document.getElementById("stage-1").classList.add("disable");
     document.getElementById("stage-2-1").classList.remove("disable");
@@ -35,36 +27,67 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById("stage-2-2").classList.add("disable");
     document.getElementById("stage-3-4").classList.remove("disable");
   });
-  function modalMoveTop() {
-    var elem = document.getElementById("modal"); 
-    var pos = -100;
-    var id = setInterval(frame, 5);
-    function frame() {
-        if (pos == 0) {
-            clearInterval(id);
-        } else {
-            pos++; 
-            elem.style.bottom = pos + '%'; 
-        }
-    }
-  }
-  function modalMoveDown() {
-    var elem = document.getElementById("modal"); 
-    var pos = 0;
-    var id = setInterval(frame, 5);
-    function frame() {
-        if (pos == 100) {
-            clearInterval(id);
-        } else {
-            pos++; 
-            elem.style.bottom = pos + '%'; 
-        }
-    }
-  }
-  var infoBtn = document.getElementById("info"),
-      closeBtn = document.getElementById("modalClose");
-  infoBtn.onclick = modalMoveTop;
-  closeBtn.onclick = modalMoveDown;
+  document.getElementById("stage-3-1-unit-1").addEventListener("click", function(){
+    document.getElementById("stage-3-1").classList.add("disable");
+    document.getElementById("stage-4-1").classList.remove("disable");
+  });
+  document.getElementById("stage-3-1-unit-2").addEventListener("click", function(){
+    document.getElementById("stage-3-1").classList.add("disable");
+    document.getElementById("stage-4-2").classList.remove("disable");
+  });
+  document.getElementById("stage-3-1-unit-3").addEventListener("click", function(){
+    document.getElementById("stage-3-1").classList.add("disable");
+    document.getElementById("stage-4-3").classList.remove("disable");
+  });
+  document.getElementById("stage-3-1-unit-4").addEventListener("click", function(){
+    document.getElementById("stage-3-1").classList.add("disable");
+    document.getElementById("stage-4-4").classList.remove("disable");
+  });
+  document.getElementById("stage-3-1-unit-5").addEventListener("click", function(){
+    document.getElementById("stage-3-1").classList.add("disable");
+    document.getElementById("stage-4-5").classList.remove("disable");
+  });
+  document.getElementById("stage-3-3-unit-1").addEventListener("click", function(){
+    document.getElementById("stage-3-3").classList.add("disable");
+    document.getElementById("stage-4-5").classList.remove("disable");
+  });
+  document.getElementById("stage-3-2-unit-1").addEventListener("click", function(){
+    document.getElementById("stage-3-2").classList.add("disable");
+    document.getElementById("stage-4-6").classList.remove("disable");
+  });
+  document.getElementById("stage-3-3-unit-2").addEventListener("click", function(){
+    document.getElementById("stage-3-3").classList.add("disable");
+    document.getElementById("stage-4-7").classList.remove("disable");
+  });
+  document.getElementById("stage-4-2-unit-1").addEventListener("click", function(){
+    document.getElementById("stage-4-2").classList.add("disable");
+    document.getElementById("stage-5-1").classList.remove("disable");
+  });
+  document.getElementById("stage-4-3-unit-1").addEventListener("click", function(){
+    document.getElementById("stage-4-3").classList.add("disable");
+    document.getElementById("stage-5-2").classList.remove("disable");
+  });
+  document.getElementById("stage-4-4-unit-1").addEventListener("click", function(){
+    document.getElementById("stage-4-4").classList.add("disable");
+    document.getElementById("stage-5-3").classList.remove("disable");
+  });
+  document.getElementById("stage-4-5-unit-1").addEventListener("click", function(){
+    document.getElementById("stage-4-5").classList.add("disable");
+    document.getElementById("stage-5-4").classList.remove("disable");
+  });
+  
+  
+  
+  
+  
+  
+  ///////////////////////////////////////////////////////////////////////////
+  document.getElementById("info").addEventListener("click", function(){
+    document.getElementById("modal").classList.add("modal-active");
+  });
+  document.getElementById("modalClose").addEventListener("click", function(){
+    document.getElementById("modal").classList.remove("modal-active");
+  });
   
   
   
